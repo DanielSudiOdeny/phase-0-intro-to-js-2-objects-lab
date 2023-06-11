d927f09c4607ca3b04ed43c807c09561569c0002
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-06-11 11:42:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+  name: "John",
+  streetAddress: "Nyayo Estate Embakasi, Nairobi",
+};
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+  return { ...employeeObject, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(
+  employeeObject,
+  key,
+  value
+) {
+  employeeObject[key] = value;
+  return employeeObject;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newObj = { ...employee };
+  delete newObj[key];
+
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
